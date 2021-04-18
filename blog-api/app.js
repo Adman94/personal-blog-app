e679b3411cd2ad122d14aded4ff017d6aada774c
Adman94 <adman00794@gmail.com>
2021-04-18 08:47:32 -0700

    init commit apr18

--- blog-api/app.js
+++ blog-api/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+// const bodyParser = require('body-parser');
+const { pool } = require('./config')
 const app = express();
-const Post = require("./api/models/posts");
+// const Post = require("./api/models/posts");
 var multer = require('multer');
 var upload = multer({ dest: 'uploads/' });
-const postsData = new Post();
+// const postsData = new Post();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -35,32 +37,49 @@ app.use((req, res, next) => {
 app.use('/uploads', express.static('uploads'));
 
 app.get("/api/posts", (req, res) => {
-
-  res.status(200).send(postsData.get());
+  pool.query('SELECT * FROM blogpost', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
 });
 
 app.get("/api/posts/:post_id", (req, res) => {
   const postId = req.params.post_id;
-  const foundPost = postsData.getIndividualBlog(postId);
-  if (foundPost) {
-    res.status(200).send(foundPost)
-  } else {
-    res.status(404).send('Not Found');
-  }
-})
+  console.log(postId);
+  pool.query(`SELECT * FROM blogpost WHERE id=${postId}`, (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows);
+  })
+});
 
-app.post("/api/posts", upload.single("post-image"), (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-  const newPost = {
-    "id": `${Date.now()}`,
-    "title": req.body.title,
-    "content": req.body.content,
-    "post_image": req.file.path,
-    "added_date": `${Date.now()}`
-  }
-  postsData.add(newPost);
-  res.status(201).send("Ok");
-})
+// app.get("/api/posts/:post_id", (req, res) => {
+//   const postId = req.params.post_id;
+//   const foundPost = postsData.getIndividualBlog(postId);
+//   if (foundPost) {
+//     res.status(200).send(foundPost)
+//   } else {
+//     res.status(404).send('Not Found');
+//   }
+// })
 
-app.listen(3000, () => console.log("Listening on http://localhost:3000"));
+// app.post("/api/posts", upload.single("post-image"), (req, res) => {
+//   console.log(req.body);
+//   console.log(req.file);
+//   const newPost = {
+//     "id": `${Date.now()}`,
+//     "title": req.body.title,
+//     "content": req.body.content,
+//     "post_image": req.file.path,
+//     "added_date": `${Date.now()}`
+//   }
+//   postsData.add(newPost);
+//   res.status(201).send("Ok");
+// })
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening at 3000`)
+})
---
+++ blog-api/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE blogpost ( 
+  ID SERIAL PRIMARY KEY, 
+  title VARCHAR(255) NOT NULL, 
+  content VARCHAR(255) NOT NULL, 
+  post_image VARCHAR(255) NOT NULL, 
+  added_date VARCHAR(255) NOT NULL
+);
+
+INSERT INTO blogpost (title, content, post_image, added_date) 
+VALUES ('This is a New Blog Post', 'This is the content!', 'uploads/post-image-1581461442199.jpg', '1581461442206');
